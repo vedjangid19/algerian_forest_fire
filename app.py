12b6b886af8499571a5d91840313c96aac1cf87c
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-09-28 03:06:32 +0530

    setup

--- app.py
+++ app.py
@@ -1,37 +1,14 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-lr= pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    print("INT FEATURES : ",int_features)
-    final_features = [np.array(int_features)]
-    prediction = lr.predict(final_features)
-    output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text='Bike Sharing Demand Count should be  {}'.format(output))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = lr.predict([np.array(list(data.values()))])
-    output = prediction[0]
-    return jsonify(output)
+@app.route('/hello', methods=['GET'])
+def helloworld():
+	if(request.method == 'GET'):
+		data = {"data": "Hello World"}
+		return jsonify(data)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+	app.run(debug=True)
